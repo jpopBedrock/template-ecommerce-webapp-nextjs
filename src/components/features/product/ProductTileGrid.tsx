4b184f58df22cb04acf1cd4ff0e21c0d296a74fa
Jon Pop <jpop@bedrock.com>
2023-08-30 10:12:42 -0400

    test build

--- src/components/features/product/ProductTileGrid.tsx
+++ src/components/features/product/ProductTileGrid.tsx
@@ -12,14 +12,16 @@ export const ProductTileGrid = ({ title, products }: ProductTileGridProps) => {
   return (
     <Container>
       {title && (
-        <Heading as="h2" mb={3}>test 
+        <Heading as="h2" mb={3}>
+          Hey Tony
           {title}
         </Heading>
       )}
       <Grid
         templateColumns={{ base: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }}
         rowGap={{ base: 6, lg: 6 }}
-        columnGap={{ base: 4, lg: 24 }}>
+        columnGap={{ base: 4, lg: 24 }}
+      >
         {products.map((product, index) => {
           return <GridItem key={index}>{product ? <ProductTile {...product} /> : null}</GridItem>;
         })}
